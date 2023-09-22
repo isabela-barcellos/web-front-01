8094cdab5b330a9768a83c3fbcbc01dfb67c2538
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 08:34:10 -0300

    Adicionando tipos de dados

---
+++ teste.js
@@ -0,0 +1,3 @@
+console.log("Teste, olá");
+
+console.log("Outro teste");
---
+++ varialvel.js
@@ -0,0 +1,9 @@
+//cria uma variavel com valor indefinido;
+let nome;
+
+//cra uma variável vazia
+let disciplina = null;
+
+let texto = "Esse é um exemplo";
+
+console.log(texto);
