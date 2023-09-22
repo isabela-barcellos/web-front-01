158dd7e9f99dfb77a31e8630fc0b52c672775ec3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:34:39 -0300

    adicionano operadores e manipulando- texto

---
+++ manipulando-texto.js
@@ -0,0 +1,10 @@
+let txt = "Estão chegando as provas";
+
+// console.log(txt.replace("provas","avaliações"));
+
+// toUpperCase - letra maiuscula
+//toLowerCase - letra minuscula
+// console.log(txt.toLowerCase());
+
+console.log(num.toFixed(2))
+
---
+++ operadore.js
@@ -0,0 +1,15 @@
+//Decloaração de variaveis
+let num1 = 10;
+let num2 = 5;
+
+//Operações matemáticas com as variaveis
+let soma = num1 +num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+// exibindo resultados no console
+console.log("O resultado da soma é :" , soma);
+console.log("O resultado da subtração é :" , subtracao);
+console.log("O resultado da multiplicação é :" , multiplicacao);
+console.log("O resultado da divisão é :" , divisao);
