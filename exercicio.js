74e1348d37171cd53539bff7571c435b8dff272e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:40:31 -0300

    adicionando exercicio e switch

---
+++ exercicio.js
@@ -0,0 +1,14 @@
+
+
+// //funcionalidade nova, verifica se o item está incluso no let
+// console.log(frutas.includes("outro"));
+let frutas = ["morango", "banana", "mamão", "melão"];
+if (frutas.includes("abacaxi")){
+    console.log('abacaxi existe no array frutas.');
+}else if (frutas.includes("pera")){
+    console.log('pera existe no array frutas');
+}else{
+    console.log('Nem pera nem abacaxi existem o array frutas.')
+}
+
+
---
+++ switch.js
@@ -0,0 +1,15 @@
+//switch éusado para colocar várias condicionais de uma vez 
+let teste = 2;
+
+switch(teste){
+    case 1:
+        console.log('foi de priemira');
+        break;
+    case 2: 
+        console.log('foi na segunda');
+        break;
+    default:
+        console.log('não foi nem na primeira e nem na segunda');
+
+}
+
