e306c4c0e00a2816aeced6853a754f6540fcc8ee
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:34:15 -0300

    adicionando novo arquivo de incremento

---
+++ exemplo1.js
@@ -0,0 +1,16 @@
+//incremento 
+let valor = 7;
+
+console.log(valor);
+
+valor = valor + 1 //alterando o valor
+//não dá para mudar o valor de uma variavel no let
+console.log(valor); 
+
+valor++;
+
+console.log(valor);
+
+valor += 2;
+
+console.log(valor);
