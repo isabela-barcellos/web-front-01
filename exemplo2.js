24852451e11e2fdf97c1e1fec7725b7d3022bee4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:47:48 -0300

    adicionando arquivo decremento

---
+++ exemplo2.js
@@ -0,0 +1,20 @@
+//Decremento (difereça é que subtrai o valor)
+let valor = 7;
+
+console.log(valor);
+
+valor--;
+
+console.log(valor);
+
+valor -= 2;
+
+console.log(valor);
+
+valor *= 2;
+
+console.log("multiplicação",valor);
+
+valor /= 2;
+
+console.log("divisão",valor);
