8c5902bce41f870e0148a2955d627ce173ecf3ed
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:49:28 -0300

    adicionando arquivo

---
+++ js/dom.js
@@ -0,0 +1,11 @@
+function teste(arg1, arg2) {
+    console.log(arg1 + arg2);
+}
+
+function imprimeNome(name){
+    console.log(`Meu nome é ${name}`);
+}
+imprimeNome("Isabela");
+imprimeNome("Barcellos");
+imprimeNome("Freire");
+teste(7, 1); //precisa chamar a função
